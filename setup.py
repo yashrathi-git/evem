e03345dffc9c305870a98442d2fe38d27275ef2e
Yash Rathi <yashrathicricket@gmail.com>
2021-03-18 08:26:33 +0530

    Revert "Removed exact version in requirements"
    This reverts commit 49732ed995314ef3784440eb0d4c7211b0759162.

--- setup.py
+++ setup.py
@@ -5,18 +5,17 @@ setup(
     version="1.0",
     packages=["event_manager"],
     include_package_data=True,
-    install_requires=[
-        "Pygments",
-        "colorama",
-        "SQLAlchemy",
-        "premailer",
-        "mistune",
-        "humanize",
-        "Jinja2",
-        "Click",
-        "beautifulsoup4",
-        "python_dateutil",
-    ],
+    install_requires=["Pygments==2.3.1",
+                      "colorama==0.4.3",
+                      "SQLAlchemy==1.3.23",
+                      "premailer==3.7.0",
+                      "mistune==2.0.0rc1",
+                      "humanize==3.2.0",
+                      "Jinja2==2.11.2",
+                      "Click==7.0",
+                      "beautifulsoup4==4.9.3",
+                      "python_dateutil==2.8.1"
+                      ],
     entry_points="""
         [console_scripts]
         evem=event_manager.__main__:cli
